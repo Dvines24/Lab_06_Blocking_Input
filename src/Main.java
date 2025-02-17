316e2420400b22ca6fd5844f2d47bf75ddb4fe86
Dylan Vines <vinesdn@mail.uc.edu>
2025-02-17 17:16:52 -0500

    Commit after CtoF

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        int celsius = 0;
+        String trash = "";
+        boolean done = false;
+        do
+        {
+            System.out.println("What is the current temperature in Celsius?");
+            if (in.hasNextInt())
+            {
+                celsius = in.nextInt();
+                System.out.println("The current temperature in Fahrenheit is " + (celsius * 1.8 + 32) + ".");
+                done = true;
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("You said the temperature was " + trash + ".");
+                System.out.println("You need to enter a valid temperature");
+            }
+        }while(!done);
     }
 }
